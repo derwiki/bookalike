031aecdc88e69a462dbba3f1b5e4cade4c5896a0
Adam Derewecki <a@der.wiki>
2023-11-15 09:54:19 -0800

    aider: Added time measurement to query() and estimated total time to complete all chunks.
    USER: Using time.monotonic(), record the time it takes to complete query() on each chunk. Use this information to logger.info an estimate for how long it will take to complete all chunks. This estimate will become better with more chunks/more data.
    
    ASSISTANT: To implement this feature, we need to modify the `main.py` file. We will add the `time` module to measure the time taken by the `query()` function. We will then use this information to estimate the total time required to process all chunks.
    
    Here are the changes:
    
    ```python
    main.py
    <<<<<<< SEARCH
    import tiktoken
    import openai
    import logging
    
    logger = logging.getLogger(__file__)
    logging.basicConfig(level=logging.INFO)
    =======
    import tiktoken
    import openai
    import logging
    import time
    
    logger = logging.getLogger(__file__)
    logging.basicConfig(level=logging.INFO)
    >>>>>>> REPLACE
    ```
    
    ```python
    main.py
    <<<<<<< SEARCH
    def query(prompt: str) -> str:
        response = openai.ChatCompletion.create(
            model="gpt-4-0611-preview",
            messages=[{"role": "user", "content": prompt}],
            temperature=0,
        )
        return response.choices[0].message.content.strip()
    =======
    def query(prompt: str) -> str:
        start_time = time.monotonic()
        response = openai.ChatCompletion.create(
            model="gpt-4-0611-preview",
            messages=[{"role": "user", "content": prompt}],
            temperature=0,
        )
        end_time = time.monotonic()
        elapsed_time = end_time - start_time
        return response.choices[0].message.content.strip(), elapsed_time
    >>>>>>> REPLACE
    ```
    
    ```python
    main.py
    <<<<<<< SEARCH
    def rewrite_and_save_chunks(chunks, output_filename):
        with open(output_filename, "w", encoding="utf-8") as output_file:
            for chunk in chunks:
                rewritten_chunk = query(
                    f"""
                    Please rewrite this text in original words, keeping the overall themes, values, lessons the same.
                    Examples should be updated.
                    Text:\n{chunk}
                """
                )
                output_file.write(rewritten_chunk + "\n\n")
    =======
    def rewrite_and_save_chunks(chunks, output_filename):
        total_time = 0
        with open(output_filename, "w", encoding="utf-8") as output_file:
            for i, chunk in enumerate(chunks):
                rewritten_chunk, elapsed_time = query(
                    f"""
                    Please rewrite this text in original words, keeping the overall themes, values, lessons the same.
                    Examples should be updated.
                    Text:\n{chunk}
                """
                )
                total_time += elapsed_time
                estimated_time = total_time / (i + 1) * len(chunks)
                logger.info(f"Estimated total time: {estimated_time} seconds")
                output_file.write(rewritten_chunk + "\n\n")
    >>>>>>> REPLACE
    ```
    
    These changes will add the time measurement to the `query()` function and use this information to estimate the total time required to process all chunks. The estimate is updated after each chunk is processed.

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import tiktoken
 import openai
 import logging
+import time
 
 logger = logging.getLogger(__file__)
 logging.basicConfig(level=logging.INFO)
 
 
-def query(prompt: str) -> str:
+def query(prompt: str) -> tuple[str, float]:
+    start_time = time.monotonic()
     response = openai.ChatCompletion.create(
         model="gpt-4-0611-preview",
         messages=[{"role": "user", "content": prompt}],
         temperature=0,
     )
-    return response.choices[0].message.content.strip()
+    end_time = time.monotonic()
+    elapsed_time = end_time - start_time
+    return response.choices[0].message.content.strip(), elapsed_time
 
 
 def load_book(filename):
@@ -42,15 +46,19 @@ def split_into_chunks(text):
 
 
 def rewrite_and_save_chunks(chunks, output_filename):
+    total_time = 0
     with open(output_filename, "w", encoding="utf-8") as output_file:
-        for chunk in chunks:
-            rewritten_chunk = query(
+        for i, chunk in enumerate(chunks):
+            rewritten_chunk, elapsed_time = query(
                 f"""
                 Please rewrite this text in original words, keeping the overall themes, values, lessons the same.
                 Examples should be updated.
                 Text:\n{chunk}
             """
             )
+            total_time += elapsed_time
+            estimated_time = total_time / (i + 1) * len(chunks)
+            logger.info(f"Estimated total time: {estimated_time} seconds")
             output_file.write(rewritten_chunk + "\n\n")
 
 
